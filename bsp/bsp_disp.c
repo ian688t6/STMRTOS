bba81212bddd0df757a44834ecc3fb5162b1b7b8
ian <ian688t6@foxmail.com>
2018-09-29 17:18:11 +0800

    [rtos] add bsp_disp_draw_rect
    Change-Id: I9fd4e9072ffa984b26933c4d9602869bfbd0f127

--- bsp/bsp_disp.c
+++ bsp/bsp_disp.c
@@ -281,6 +281,16 @@ void bsp_disp_draw_line(uint16_t us_x1, uint16_t us_y1, uint16_t us_x2, uint16_t
 	return;
 }
 
+void bsp_disp_draw_rect(uint16_t us_x1, uint16_t us_y1, uint16_t us_x2, uint16_t us_y2)
+{
+	bsp_disp_draw_line(us_x1, us_y1, us_x2, us_y1);
+	bsp_disp_draw_line(us_x1, us_y1, us_x1, us_y2);
+	bsp_disp_draw_line(us_x1, us_y2, us_x2, us_y2);
+	bsp_disp_draw_line(us_x2, us_y1, us_x2, us_y2);
+	
+	return;
+}
+
 void bsp_disp_clear(uint16_t us_color)
 {
 	int32_t x = 0;
--- bsp/bsp_disp.h
+++ bsp/bsp_disp.h
@@ -7,6 +7,8 @@ extern void bsp_disp_draw_point(uint16_t us_x, uint16_t us_y, uint16_t us_color)
 
 extern void bsp_disp_draw_line(uint16_t us_x1, uint16_t us_y1, uint16_t us_x2, uint16_t us_y2);
 
+extern void bsp_disp_draw_rect(uint16_t us_x1, uint16_t us_y1, uint16_t us_x2, uint16_t us_y2);
+
 extern uint16_t bsp_disp_id(void);
 
 #endif
