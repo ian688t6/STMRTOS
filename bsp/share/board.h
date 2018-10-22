033eaaa34128ac418aff6898ac7b3412d725786d
ian <ian688t6@foxmail.com>
2018-10-22 16:10:29 +0800

    [rtos] add wifi_mcu
    Change-Id: If8ccb93b3262328ca686c225d4457d99270eca49

--- bsp/share/board.h
+++ bsp/share/board.h
@@ -4,6 +4,7 @@
 #include "board_lcd.h"
 #include "board_gpio.h"
 #include "board_tp.h"
+#include "board_wifi.h"
 
 typedef struct
 {
@@ -11,6 +12,7 @@ typedef struct
 	board_gpio_t	st_backlight_gpio;
 	lcd_panel_t *(*pf_lcd_panel_get)(void);
 	touch_panel_t *(*pf_touch_panel_get)(void);
+	wifi_mcu_t *(*pf_wifi_mcu_get)(void);
 } board_t;
 
 extern board_t *board_get(void);
--- bsp/share/board_wifi.h
+++ bsp/share/board_wifi.h
@@ -1,4 +1,12 @@
 #ifndef __BOARD_WIFI_H__
 #define __BOARD_WIFI_H__
 
+#include "bsp_uart.h"
+typedef struct
+{
+	void (*init)(bsp_uart_s *pst_uart);
+} wifi_mcu_t;
+
+extern wifi_mcu_t *wifi_mcu_get(void);
+
 #endif
